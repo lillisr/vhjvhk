1bf34f7be4676ef61ccbcb891c0c6d22806fa5ca
celinehuber <celine.huber@mail.de>
2023-12-06 15:09:02 +0100

    test

--- BackendTest.php
+++ BackendTest.php
@@ -1,14 +1,14 @@
 <?php
 require("start.php");
 $service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
-//var_dump($service->test());
-//var_dump($service->register("Test123", "Test456"));
-//var_dump($service->login("Jerry", "87654321"));
-//var_dump($service->loadUser("Test123"));
-//probem hier: man muss php array übergeben
-//var_dump($service->saveUser("Test123"));
+var_dump($service->test());
+var_dump($service->register("Test123", "Test456"));
+var_dump($service->login("Jerry", "87654321"));
+var_dump($service->loadUser("Test123"));
+//problem hier: man muss php array übergeben
+var_dump($service->saveUser("Test123"));
 var_dump($service->loadMessages("Test123"));
-/*var_dump($service->loadFriends());
+var_dump($service->loadFriends());
 var_dump($service->loadUsers());
 var_dump($service->sendMessage("Test123"));
 var_dump($service->friendRequest("Test123"));
@@ -17,5 +17,5 @@ var_dump($service->friendDismiss("Test123"));
 var_dump($service->removeFriend("Test123"));
 var_dump($service->userExists("Test123"));
 var_dump($service->getUnread());
-*/
+
 ?>
