bc77de1f0c3b4dbd2b3f4db065e2accce5a0b43c
celinehuber <celine.huber@mail.de>
2023-12-13 15:44:52 +0100

    remove friend

--- chat.php
+++ chat.php
@@ -9,7 +9,7 @@ if (!isset($_GET['friend']) || empty($_GET['friend'])) {
     header("Location: friends.php");
     exit();
 }
-$username=$_GET['friend'];
+$username = $_GET['friend'];
 var_dump($username);
 
 ?>
@@ -17,36 +17,40 @@ var_dump($username);
 
 <!doctype html>
 <html>
-    <head>
-        <title>chat</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="allgemien.css">
-        
-    </head> 
-    <body>
-        <div class="container">
-        <form class="chat">
+
+<head>
+    <title>chat</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="allgemien.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <form class="chat" method="GET">
             <h1 id="chatPartner"> </h1>
             <a href="friends.php" target="_back"> >Back </a> |
-            <a href="profile.php?friend=<?php echo $username ?>" target="_Profile"> Profile </a> | 
-            <a href="friends.php" target="RemoveFriend" id="Achtung"> Remove Friend </a><br>
+            <a href="profile.php?friend=<?php echo $username ?>" target="_Profile"> Profile </a> |
+            <a href="friends.php?friend=<?php echo $_GET['friend'] ?>&action=remove-friend" id="Achtung"> Remove Friend
+            </a><br>
             <hr />
             <div class="chatlist">
-                <div> 
+                <div>
                     <ul id="chatMessages">
-                        
+
                     </ul>
                 </div>
             </div>
             <hr />
             <div class="newMessage">
                 <input class="Message" type="text" placeholder="New Message" id="messageField">
-            <div class="sendButton">
-                <input type="button" class="greyButton" onclick="sendMessages()" value="Send">
-            </div>
+                <div class="sendButton">
+                    <input type="button" class="greyButton" onclick="sendMessages()" value="Send">
+                </div>
         </form>
-        </div>
-        
-    </body>
-    <script src="chat.js"></script>
+    </div>
+
+</body>
+<script src="chat.js"></script>
+
 </html>
